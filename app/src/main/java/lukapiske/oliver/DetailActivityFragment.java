3a81e6b9c6fddae32df692a6c3a215a8db4c49db
Luky <lukapiske@gmail.com>
2016-04-30 22:50:21 +0200

    Implicit intent - ShareActionProvider implementation

--- app/src/main/java/lukapiske/oliver/DetailActivityFragment.java
+++ app/src/main/java/lukapiske/oliver/DetailActivityFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,6 +21,11 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #Oliver";
+    private String message;
+
     public DetailActivityFragment() {
     }
 
@@ -32,7 +39,29 @@ public class DetailActivityFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // TODO Add your menu entries here
         inflater.inflate(R.menu.menu_detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                message + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 
     @Override
@@ -55,12 +84,13 @@ public class DetailActivityFragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_detail, container, false);
         Log.v("WORK_CICLE", "DetailActivityFragment onCreateView");
         Intent intent = getActivity().getIntent();
-        String message = intent.getStringExtra(Intent.EXTRA_TEXT);
-        Log.v("WORK_CICLE", "Intent in fragment"+message);
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            message = intent.getStringExtra(Intent.EXTRA_TEXT);
+            Log.v("WORK_CICLE", "Intent in fragment" + message);
 
-
-        TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-        textView.setText(message);
+            TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
+            textView.setText(message);
+        }
 
         return rootView;
     }
